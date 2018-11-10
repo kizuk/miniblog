e5c183d9a3d3653a120d94015027d0fa37a96575
Negishi Kazuki <h2dhe2yc@icloud.com>
2018-11-10 12:51:29 +0900

    forth commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	def show
 	 user = User.find(params[:id])
 	 @nickname = user.nickname
-	 @tweets = Tweet.where(params[:user_id]).order('created_at DESC').page(params[:page]).per(5)
+	 @tweets = Tweet.where(user_id: user).order('created_at DESC').page(params[:page]).per(5)
     end
 
 end
